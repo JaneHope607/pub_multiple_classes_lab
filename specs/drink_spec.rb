5e2a6c230b3261e456381f88102c5d67ff22ac58
Helena <inger.helena@gmail.com>
2020-06-24 10:40:12 +0100

    created drink class

---
+++ specs/drink_spec.rb
@@ -0,0 +1,13 @@
+require('minitest/autorun')
+require('minitest/reporters')
+Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
+
+require_relative('../drink')
+
+class DrinkTest < MiniTest::Test
+
+
+
+
+    
+end
