ffbc59fa25ddd80bd928c4b623e24597d756467a
Helena <inger.helena@gmail.com>
2020-06-24 13:41:19 +0100

    unlinked pub from customer spec

--- specs/customer_spec.rb
+++ specs/customer_spec.rb
@@ -3,7 +3,6 @@ require('minitest/reporters')
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 require_relative('../customer')
-require_relative('../pub')
 require_relative('../drink')
 
 class CustomerTest < MiniTest::Test
@@ -13,8 +12,6 @@ class CustomerTest < MiniTest::Test
         @drink2 = Drink.new("Corona", 4)
         @drink3 = Drink.new("Jack Daniels", 2)
         @drink4 = Drink.new("Strongbow", 3)
-        @bar = [@drink1, @drink2, @drink3, @drink4]
-        @pub = Pub.new("Three Broomsticks", 1000, @bar)
         @customer1 = Customer.new("John", 20, 35)
         @customer2 = Customer.new("Bob", 55, 16)
     end
@@ -32,11 +29,11 @@ class CustomerTest < MiniTest::Test
         assert_equal(15, @customer1.wallet)
     end
 
-    def test_customer_can_buy_drink()
-        @customer1.buy_drink_from_pub(@pub, @drink1)
-        assert_equal(15, @customer1.wallet)
-        assert_equal(1005, @pub.till)
-    end
+    # def test_customer_can_buy_drink()
+    #     @customer1.buy_drink_from_pub(@pub, @drink1)
+    #     assert_equal(15, @customer1.wallet)
+    #     assert_equal(1005, @pub.till)
+    # end
 
     def test_is_customer_legal_age_true()
         assert_equal(true, @customer1.customer_legal_age())
