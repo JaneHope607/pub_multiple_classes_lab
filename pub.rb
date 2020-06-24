d6eaac3770fa9a73494114e63e1a9af70b2b0895
Helena <inger.helena@gmail.com>
2020-06-24 10:29:48 +0100

    created pub class

---
+++ pub.rb
@@ -0,0 +1,9 @@
+class Pub
+
+    def initialize(name, till, drinks)
+        @name = name
+        @till = till
+        @drinks = drinks
+    end
+
+end
---
+++ specs/pub_spec.rb
@@ -0,0 +1,10 @@
+require('minitest/autorun')
+require('minitest/reporters')
+Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
+
+require_relative('../pub')
+
+class PubTest < MiniTest::Test
+
+
+end
