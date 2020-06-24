7e3893b307dc964e750960a40f7081a757af1963
Helena <inger.helena@gmail.com>
2020-06-24 14:26:08 +0100

    created food class, test check name pass

---
+++ specs/food_spec.rb
@@ -0,0 +1,19 @@
+require('minitest/autorun')
+require('minitest/reporters')
+Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
+
+require_relative('../food')
+
+class FoodTest < MiniTest::Test
+
+    def setup()
+        @food = Food.new("chips", 5, 1)
+    end
+
+    def test_check_name()
+        assert_equal("chips", @food.name)
+    end
+
+    # def test_check_price()
+
+end
